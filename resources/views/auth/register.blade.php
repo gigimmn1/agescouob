156171175dd1d9ae6035d5c9fcde1e5e53a55382
justinho Mikala <mikalajuste@gmail.com>
2020-07-31 16:22:31 +0200

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,38 +13,54 @@
           <div class="col-lg-7">
             <div class="p-5">
               <div class="text-center">
-                <h1 class="h4 text-gray-900 mb-4">s'inscrire!</h1>
+                <h1 class="h4 text-gray-900 mb-4">S'inscrire!</h1>
               </div>
-              <form class="user">
+              <form class="user" method="POST" action="{{ route('register') }}">
+                @csrf
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="votre nom">
+                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror form-control-user" placeholder="votre nom" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus >
+                    @error('name')
+                      <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                      </span>
+                    @enderror
                   </div>
                   
                 </div>
                 <div class="form-group">
-                  <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="votre mail">
+                  <input id="email" type="email" class="form-control @error('email') is-invalid @enderror form-control-user" placeholder="votre adresse email" name="email" value="{{ old('email') }}" required autocomplete="email">
+                  @error('email')
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                      </span>
+                  @enderror
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="saisir un mot de passe">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror form-control-user" placeholder="Saisir un mot de passe" name="password" required autocomplete="new-password">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                   </div>
                   <div class="col-sm-6">
-                    <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="confirmer mot de passe">
+                    <input id="password-confirm" type="password" class="form-control form-control-user" placeholder="confirmer mot de passe" name="password_confirmation" required autocomplete="new-password">
                   </div>
                 </div>
-                <a href="login.html" class="btn btn-primary btn-user btn-block">
-                  s'inscrire
-                </a>
-                <hr>
+
+                <button type="submit" class="btn btn-primary btn-user btn-block">S'inscrire</button>
                 
               </form>
               <hr>
               <div class="text-center">
-                <a class="small" href="forgot-password.html">Forgot Password?</a>
+                @if (Route::has('password.request'))
+                   <a class="small" href="{{ route('password.request') }}">Mot de passe oublié ?</a>
+                @endif
               </div>
               <div class="text-center">
-                <a class="small" href="login.html">Already have an account? Login!</a>
+                <a class="small" href="{{ route('login') }}">Vous avez déjà un compte? S'identifier!</a>
               </div>
             </div>
           </div>
