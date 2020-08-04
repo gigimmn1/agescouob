079623c59ba9b4ff7db2e3325cf2fe04123fa4d1
justinho Mikala <mikalajuste@gmail.com>
2020-08-04 17:40:11 +0200

    controllers et models (evaluations, inscriptions)

--- app/note.php
+++ app/note.php
@@ -4,12 +4,44 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class note extends Model
+class Note extends Model
 {
-    //
+    /**
+     * The primary key associated with the table.
+     *
+     * @var int
+    */
+    protected $primaryKey = 'id_Note';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+    */
     protected $fillable = [
-        'id',
-        'libelle_note'
+        'libelle_Note',
+        'valeur_Note',
+        'id_Evaluation',
+        'id_Inscription'
     ];
+
+    /**
+     * Get the inscription that owns the note.
+    */
+    public function inscription()
+    {
+        return $this->belongsTo('App\Inscription', 'id_Inscription', 'id_Inscription');
+    }
+
+    /**
+     * Get the evaluation that owns the note.
+    */
+    public function evaluation()
+    {
+        return $this->belongsTo('App\Evaluation', 'id_Evaluation', 'id_Evaluation');
+    }
+    
+
+ 
 
 }
