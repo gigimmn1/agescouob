2c99373a0d803eb1c3b1006b0368263655a51c3a
0BR-A <68922836+0BR-A@users.noreply.github.com>
2020-08-22 00:59:30 +0200

    AGESCOUOB

---
+++ app/Parent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Parent extends Model
+{
+    //
+    protected $table = 'parents';
+    
+    public $timestamps = false;
+}
